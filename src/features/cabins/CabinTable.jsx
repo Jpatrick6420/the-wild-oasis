625327717c339ef8479f2e27d82f42435680eb91
Justin Patrick <jpatrick6420@gmail.com>
2024-02-08 07:12:58 -0700

    added the filter function into the cabins component

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -6,6 +6,7 @@ import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table2";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 // const Table = styled.div`
 //   border: 1px solid var(--color-grey-200);
@@ -33,8 +34,24 @@ const TableHeader = styled.header`
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+  console.log(filterValue);
+
+  let filteredCabins;
+  if (filterValue === "all") {
+    filteredCabins = cabins;
+  }
+  if (filterValue === "no-discount") {
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  }
+  if (filterValue === "with-discount") {
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  }
+
   return (
     <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
       <Table.Header>
@@ -46,7 +63,7 @@ function CabinTable() {
         <div></div>
       </Table.Header>
       <Table.Body
-        data={cabins}
+        data={filteredCabins}
         render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
       />
     </Table>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -6,6 +6,7 @@ import CabinTable from "../features/cabins/CabinTable.jsx";
 // import Button from "../ui/Button.jsx";
 // import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
 import AddCabin from "../features/cabins/AddCabin.jsx";
+import CabinTableOperations from "../features/cabins/CabinTableOperations.jsx";
 
 function Cabins() {
   // const [showForm, setShowForm] = useState(false);
@@ -18,11 +19,12 @@ function Cabins() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>TEST</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />
         <AddCabin />
+        <p>Filter/Sort</p>
 
         {/* <Button onClick={() => setShowForm((show) => !show)}>
           {showForm ? "Close Form" : "Add new Cabin"}
