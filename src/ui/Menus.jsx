44902809961d0acf23cfad286b61a6431d255208
Justin Patrick <jpatrick6420@gmail.com>
2024-03-23 08:16:36 -0600

    added error boundary handling

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenuContext);
 
   function handleClick(e) {
+    console.log("click");
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
